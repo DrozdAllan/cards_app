3f567a2b57b12bbfdf02aacd96e49fb3d46ad5e2
Allan <drozdallan@gmail.com>
2023-09-04 01:35:57 +0200

    deuz

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,47 @@
 <template>
 	<q-page padding>
-		<p>cards</p>
-		<p v-for="n in 5" :key="n">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore
-			vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi
-			perferendis voluptate?
-		</p>
+		<div class="row">
+			<div v-for="card in cards" class="col-3">
+				<q-card>
+					<q-card-section>
+						<div class="text-h6">{{ card.name }}</div>
+						<div class="text-subtitle2">{{ card.type }}</div>
+					</q-card-section>
+
+					<q-card-section class="ellipsis-2-lines">
+						{{ card.desc }}
+					</q-card-section>
+
+					<q-separator dark />
+
+					<q-card-actions>
+						<q-btn flat>{{ card.atk }}</q-btn>
+						<q-btn flat>{{ card.def }}</q-btn>
+					</q-card-actions>
+				</q-card>
+			</div>
+		</div>
 	</q-page>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+
+const cards = ref([]);
+
+onMounted(async () => {
+	try {
+		const response = await fetch(import.meta.env.VITE_API_ENDPOINT + '/cards', {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			}
+		});
+		cards.value = await response.json();
+	} catch (error) {
+		console.log(error);
+		return error
+	}
+})
+
 </script>
